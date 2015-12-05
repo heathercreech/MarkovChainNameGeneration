c285957acbd6ac670c83bd5bbc5ad5e1328cd6f2
Seth Creech <sacreech@moreheadstate.edu>
2015-12-05 17:28:40 -0500

    Added license information.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 #include <iostream>
 
+
 int main(int argc, char* argv[]) {
 
-	markov::ChainVector<std::string> chains = markov::getChainsFromFile<std::string>("test.json");
-	printf("<Returned Chain>\nSrc:\t%s\nDest:\t%s\nProb:\t%f\n", chains[1].src.c_str(), chains[1].dest.c_str(), chains[1].probability);
+	markov::ChainVector<std::string> chains = markov::getChainsFromFile<std::string>("data.json");
+	printf("<Returned Chain>\nSrc:\t%s\nDest:\t%s\nProb:\t%f\n", chains[0].src.c_str(), chains[0].dest.c_str(), chains[0].probability);
 }
