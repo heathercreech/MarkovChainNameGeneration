d9a30faad844e53b3f2147b5f8f73a873e0a922c
Seth Creech <sacreech@moreheadstate.edu>
2015-12-06 23:34:26 -0500

    Added some temporary logic to main. Used to generate a file 100 names long.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 
 #include <vector>
 #include <iostream>
+#include <ctime>
 
+unsigned int count = 100;
+unsigned int min_iter = 3;
+unsigned int max_iter = 5;
 
 int main(int argc, char* argv[]) {
+	srand(time(NULL));
 
 	markov::ChainVector<std::string> chains = markov::getChainsFromFile<std::string>("data.json");
-	printf("<Returned Chain>\nSrc:\t%s\nDest:\t%s\nProb:\t%f\n", chains[0].src.c_str(), chains[0].dest.c_str(), chains[0].probability);
+	NameGenerator name_gen(chains, 2, 8);
+	
+	std::ofstream output_file("out.txt");
+	for (unsigned int i = 0; i < 100; i++) {
+		output_file << name_gen.generate() << "\n";
+		name_gen.calculateStartChars();
+	}
+	output_file.close();
 }
